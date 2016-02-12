8f4b9a78ed23e0c6d8582dbd1f5759a6bc262920
Ryan Thompson <ryan@pyrocms.com>
2016-02-12 09:44:19 -0600

    Conflict resolution

---
+++ resources/js/input.js
@@ -0,0 +1,9 @@
+$(function () {
+
+    // Initialize form switches
+    $('.language-field_type select').each(function () {
+        $(this).select2({
+            theme: 'bootstrap'
+        });
+    });
+});
