e03d83b96ab665061efae30a915217724c9fbf20
Ryan Thompson <ryan@pyrocms.com>
2016-02-12 14:31:37 -0600

    C-select

--- resources/js/input.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-
-    // Initialize form switches
-    $('.language-field_type select').each(function () {
-        $(this).select2({
-            theme: 'bootstrap'
-        });
-    });
-});
